5e15c8bbecef7514e31b5ac17263af7eb915b80d
Frédéric NOËL <contact@fredericnoel.com>
2020-02-28 10:23:19 +0100

    Formulaire de saisie du nombre à analyser

---
+++ includes/frmFormArray.php
@@ -0,0 +1,11 @@
+<form action="index.php" method="post">
+    <div>
+        <label for="valeur">Veuillez saisir un nombre entier : </label>
+        <input type="text" name="valeur" id="valeur" />
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Valider" />
+    </div>
+    <input type="hidden" name="frmFormArray" />
+</form>
