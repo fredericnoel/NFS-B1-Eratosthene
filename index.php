b638f9edf78e0576ed76fc856a60ac751bf19b6a
Frédéric NOËL <contact@fredericnoel.com>
2020-03-10 11:48:48 +0100

    Remplacement de la fonction generateArray() par range()

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once './functions/eratosthene.php';
 
 if (isset($_POST['frmFormArray'])) {
     $n = isset($_POST['valeur']) ? $_POST['valeur'] : '';
-    $n = generateArray($n);
+    $n = range(2, $n);
 
     $resultat = eratosthene($n);
 }
